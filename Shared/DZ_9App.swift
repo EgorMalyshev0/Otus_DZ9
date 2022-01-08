e25b509d9e21178b13d246b704306b43e383481b
Egor Malyshev <egor.malyshev@umbrellait.com>
2022-01-08 22:42:47 +0300

    Initial Commit

---
+++ Shared/DZ_9App.swift
@@ -0,0 +1,17 @@
+//
+//  DZ_9App.swift
+//  Shared
+//
+//  Created by Egor Malyshev on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct DZ_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
