83846b201d5ba38e09aac263fa8a07088e17f7cb
Egor Malyshev <egor.malyshev@umbrellait.com>
2022-01-09 21:55:46 +0300

    added watchos target

---
+++ DZ№9 (watchOS) WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  DZ№9 (watchOS) WatchKit Extension
+//
+//  Created by Egor Malyshev on 09.01.2022.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
